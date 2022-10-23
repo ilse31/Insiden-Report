c6ee7308c73c855686b316c3a7e45116c5022e2e
ilse31 <ilhamprazsetyo33@gmail.com>
2022-10-23 10:04:42 +0700

    init routes

---
+++ routes/Routes.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Routes = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Routes</Text>
+    </View>
+  );
+};
+
+export default Routes;
